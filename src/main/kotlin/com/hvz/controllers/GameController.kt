bbbd9aa651fca01cbeedf8281eee0f09f056595f
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-26 15:47:21 +0200

    Feat: added game CRUD operations

--- src/main/kotlin/com/hvz/controllers/GameController.kt
+++ src/main/kotlin/com/hvz/controllers/GameController.kt
@@ -1,9 +1,8 @@
 package com.hvz.controllers
 
+import com.hvz.models.Game
 import com.hvz.services.game.GameService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["api/v1/"])
@@ -11,4 +10,16 @@ class GameController(val gameService: GameService) {
 
     @GetMapping("games")
     fun findAll() = gameService.findAll()
+
+    @GetMapping("games/{id}")
+    fun findById(@PathVariable id: Int) = gameService.findById(id)
+
+    @PutMapping("games/{id}")
+    fun update(@PathVariable id: Int, @RequestBody game: Game) = gameService.update(game)
+
+    @DeleteMapping("games/{id}")
+    fun deleteById(@PathVariable id: Int) = gameService.deleteById(id)
+
+    @PostMapping("games")
+    fun addGame(@RequestBody game: Game) = gameService.add(game)
 }
