de6465db6f1b66ddad7a52985613ee766f3c76e2
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-26 13:26:04 +0200

    Feat(PlayerController): added POST-mapping

--- src/main/kotlin/com/hvz/controllers/PlayerController.kt
+++ src/main/kotlin/com/hvz/controllers/PlayerController.kt
@@ -5,7 +5,9 @@ import com.hvz.services.player.PlayerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,4 +27,7 @@ class PlayerController {
 
     @DeleteMapping("players/{id}")
     fun deleteById(@PathVariable id: Int) = playerService.deleteById(id)
+
+    @PostMapping("players")
+    fun addPlayer(@RequestBody player: Player) = playerService.add(player)
 }
