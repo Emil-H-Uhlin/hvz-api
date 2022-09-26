b189c9f0fd9d5e5eecd7e72588292357ac6619d3
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-26 10:45:09 +0200

    Feat: removed lombok dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    
 }
 
 tasks.withType<KotlinCompile> {
