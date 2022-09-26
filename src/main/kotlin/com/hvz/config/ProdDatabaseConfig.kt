5b5f6f80fb0bdfb9e8cacbabfb08992642213d10
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-26 11:11:04 +0200

    Feat: added datasource configuration

---
+++ src/main/kotlin/com/hvz/config/ProdDatabaseConfig.kt
@@ -0,0 +1,33 @@
+package com.hvz.config
+
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import java.net.URI
+
+@Configuration
+@Profile("prod")
+class ProdDatabaseConfig
+
+@Bean
+fun dataSource() {
+    val dbUri = URI(System.getenv("DATABASE_URL"))
+
+    var usr: String
+    var pwd: String
+
+    val dbUrl = "jdbc:postgresql://" + dbUri.host + ":" + dbUri.port + dbUri.path
+
+    with (dbUri.userInfo.split(":")) {
+        usr = this[0]
+        pwd = this[1]
+
+    }
+
+    return with(DataSourceBuilder.create()) {
+        url(dbUrl)
+        username(usr)
+        password(pwd)
+    }
+}
