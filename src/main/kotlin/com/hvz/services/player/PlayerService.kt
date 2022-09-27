72ccdd0f8796efa63feb5147053f7c6e7767005e
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-27 13:27:06 +0200

    Feat: added find-by-bitecode

--- src/main/kotlin/com/hvz/services/player/PlayerService.kt
+++ src/main/kotlin/com/hvz/services/player/PlayerService.kt
@@ -3,4 +3,6 @@ package com.hvz.services.player
 import com.hvz.models.Player
 import com.hvz.services.CrudService
 
-interface PlayerService : CrudService<Player, Int>
+interface PlayerService : CrudService<Player, Int> {
+    fun findByBiteCode(biteCode: String): Player?
+}
