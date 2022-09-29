332bcb746e90674c62d5e43cf415c64742721848
Emil Uhlin <emil.uhlin@se.experis.com>
2022-09-29 09:23:34 +0200

    Feat: re-added openapi for documentation using postman

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
 }
 
 tasks.withType<KotlinCompile> {
