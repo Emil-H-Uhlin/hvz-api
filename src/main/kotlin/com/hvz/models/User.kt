eb5ce7978f3fefc24a2e4ebc3a3ec6c3d93c0243
Emil Uhlin <emil.uhlin@se.experis.com>
2022-10-07 12:49:08 +0200

    Feat: added user DTOS

--- src/main/kotlin/com/hvz/models/User.kt
+++ src/main/kotlin/com/hvz/models/User.kt
@@ -1,5 +1,6 @@
 package com.hvz.models
 
+import org.springframework.security.oauth2.jwt.Jwt
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToMany
@@ -8,7 +9,21 @@ import javax.persistence.OneToMany
 data class User(
     @Id
     val uid: String,
+    val name: String,
+    val email: String,
 ) {
     @OneToMany(mappedBy = "user")
     val players: Collection<Player> = setOf()
+
+    fun toReadDto() = UserReadDTO(uid, name, email, players.map { it.id })
 }
+
+data class UserAddDTO(val name: String, val email: String) {
+    fun toEntity(jwt: Jwt) = User(jwt.claims["sub"] as String, name, email)
+}
+
+data class UserReadDTO(val uid: String,
+                       val name: String,
+                       val email: String,
+                       val players: Collection<Int>,
+)
