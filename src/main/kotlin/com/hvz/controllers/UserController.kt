2da897ce4edd1dfe8d34eacf363a2760b447891b
Emil Uhlin <emil.uhlin@se.experis.com>
2022-10-07 12:57:39 +0200

    Feat: added endpoint for getting all users in a game

--- src/main/kotlin/com/hvz/controllers/UserController.kt
+++ src/main/kotlin/com/hvz/controllers/UserController.kt
@@ -1,7 +1,9 @@
 package com.hvz.controllers
 
+import com.hvz.exceptions.GameNotFoundException
 import com.hvz.exceptions.UserNotFoundException
 import com.hvz.models.UserAddDTO
+import com.hvz.services.game.GameService
 import com.hvz.services.user.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -19,7 +21,9 @@ import java.net.URI
 @RestController
 @RequestMapping
 @CrossOrigin(origins = ["*"])
-class UserController(private val userService: UserService) {
+class UserController(private val userService: UserService,
+                     private val gameService: GameService
+) {
 
     @DeleteMapping("api/v1/users/{id}")
     fun deleteById(@PathVariable(name = "id") uid: String): ResponseEntity<Any> {
@@ -55,6 +59,22 @@ class UserController(private val userService: UserService) {
             val user = userService.findById(uid)
             return ResponseEntity.ok(user.toReadDto())
         } catch (userNotFoundException: UserNotFoundException) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping("api/v1/games/{game_id}/users")
+    fun findByGameId(@PathVariable(name = "game_id") gameId: Int): ResponseEntity<Any> {
+
+        return try {
+            val game = gameService.findById(gameId)
+
+            val users = userService.findAll().filter { user ->
+                game.players.find { it.user!!.uid == user.uid } != null
+            }
+
+            return ResponseEntity.ok(users.map { it.toReadDto() })
+        } catch (gameNotFoundException: GameNotFoundException) {
             ResponseEntity.notFound().build()
         }
     }
