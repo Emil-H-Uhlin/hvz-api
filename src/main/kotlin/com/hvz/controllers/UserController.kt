2c87166033f78873d5e388b4d64e17d5f53440b2
Emil Uhlin <emil.uhlin@se.experis.com>
2022-10-07 12:51:12 +0200

    Feat: added user controller

---
+++ src/main/kotlin/com/hvz/controllers/UserController.kt
@@ -0,0 +1,61 @@
+package com.hvz.controllers
+
+import com.hvz.exceptions.UserNotFoundException
+import com.hvz.models.UserAddDTO
+import com.hvz.services.user.UserService
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = ["*"])
+class UserController(private val userService: UserService) {
+
+    @DeleteMapping("api/v1/users/{id}")
+    fun deleteById(@PathVariable(name = "id") uid: String): ResponseEntity<Any> {
+
+        return try {
+            userService.findById(uid)
+            userService.deleteById(uid)
+
+            ResponseEntity.noContent().build()
+        } catch (userNotFoundException: UserNotFoundException) {
+            ResponseEntity.badRequest().build()
+        }
+    }
+
+    @PostMapping("/register")
+    fun addUser(@AuthenticationPrincipal jwt: Jwt,
+                @RequestBody dto: UserAddDTO): ResponseEntity<Any> {
+
+        val user = userService.add(dto.toEntity(jwt))
+
+        val uri = URI.create("api/v1/users/${user.uid}")
+
+        return ResponseEntity.created(uri).build()
+    }
+
+    @GetMapping("api/v1/users")
+    fun findAll() = ResponseEntity.ok(userService.findAll().map { it.toReadDto() })
+
+    @GetMapping("api/v1/users/{id}")
+    fun findById(@PathVariable(name = "id") uid: String): ResponseEntity<Any> {
+
+        return try {
+            val user = userService.findById(uid)
+            return ResponseEntity.ok(user.toReadDto())
+        } catch (userNotFoundException: UserNotFoundException) {
+            ResponseEntity.notFound().build()
+        }
+    }
+}
